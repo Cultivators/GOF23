33647d33d7aa0921ca77bc84b3a288c6ba6c799b
cultivator <wuxinjie13450@gmail.com>
2015-12-13 23:43:52 +0800

    第二次提交
    Signed-off-by: cultivator <wuxinjie13450@gmail.com>

--- src/cn/cultivator/sigleton/Singleton_1.java
+++ src/cn/cultivator/sigleton/Singleton_1.java
@@ -1,4 +1,15 @@
 package cn.cultivator.sigleton;
 
 public class Singleton_1 {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
